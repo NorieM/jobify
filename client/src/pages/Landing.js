7c1e8f3628ab2d250c76a0a5d737a50c2f997915
Norie <norie.macewan@tiscali.co.uk>
2023-04-07 21:00:29 +0100

    Finished Landing page section

--- client/src/pages/Landing.js
+++ client/src/pages/Landing.js
@@ -1,9 +1,10 @@
-import logo from '../assets/images/logo.svg';
-import main from '../assets/images/main.svg'
+import logo from "../assets/images/logo.svg";
+import main from "../assets/images/main.svg";
+import Wrapper from "../assets/wrappers/LandingPage";
 
 const Landing = () => {
   return (
-    <main>
+    <Wrapper>
       <nav>
         <img src={logo} alt="Jobify" className="logo" />
       </nav>
@@ -13,23 +14,20 @@ const Landing = () => {
             job <span>tracking</span> App
           </h1>
           <p>
-            I'm baby celiac occupy mukbang salvia, 
-            drinking vinegar normcore DIY. Bicycle 
-            rights seitan vinyl cardigan. Gastropub 
-            selvage whatever, beard big mood knausgaard 
-            kombucha pork belly same bodega boys iPhone 
-            locavore pour-over. Prism pabst butcher, pug 
-            squid vibecession selfies
+            I'm baby celiac occupy mukbang salvia, drinking vinegar normcore
+            DIY. Bicycle rights seitan vinyl cardigan. Gastropub selvage
+            whatever, beard big mood knausgaard kombucha pork belly same bodega
+            boys iPhone locavore pour-over. Prism pabst butcher, pug squid
+            vibecession selfies
           </p>
-          <button className="btn btn-hero">
-            Login/Register
-          </button>
+          <button className="btn btn-hero">Login/Register</button>
         </div>
         <img src={main} alt="job hunt" className="img main-img" />
       </div>
-    </main>
-  )
-}
+    </Wrapper>
+  );
+};
 
-export default Landing
 
+
+export default Landing;
